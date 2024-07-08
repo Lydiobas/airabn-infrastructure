fc56d6aafd0b6dcf19b74684d5e0d15c8d9bb0ed
lydia <lydia@gmail.com>
2024-07-08 01:03:54 -0500

    error

--- ec2.tf
+++ ec2.tf
@@ -2,5 +2,10 @@ resource "aws_instance" "Jenkins" {
 	ami = var.ami
 	instance_type = var.instance_type
 	tags = var.tags
+	monitoring = true
+	ebs_optimized = true
+	root_block_device {
+	  encrypted = true
+	}
 }
 
